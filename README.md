aa17af70df3d1047b9c62c898f0e8f19c90e67c4
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-05-24 15:20:41 -0600

    created the scaffolding for the project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # youtube_clone
 A clone of the front end of Youtube.
+
+Dependencies:
+-npm install @mui/material @emotion/react @emotion/styled
+-npm install @mui/icons-material
+
---
+++ youtube-clone/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+import MenuSharpIcon from '@mui/icons-material/MenuSharp';
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>This is the HEADER</h1>
+      <MenuSharpIcon />
+    </div>
+  )
+}
+
+export default Header
