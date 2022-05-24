49cb7bf471ea2366325c3e56993f5e98b8f5778f
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-05-24 14:35:38 -0600

    created a firebase file

---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB1EocLZlLutZDpieLEym2UPiRa05pie2k",
+  authDomain: "clone-48b73.firebaseapp.com",
+  projectId: "clone-48b73",
+  storageBucket: "clone-48b73.appspot.com",
+  messagingSenderId: "255936910395",
+  appId: "1:255936910395:web:1552971ece7be2c38fb81e",
+  measurementId: "G-MFZB3R4T89"
+};
