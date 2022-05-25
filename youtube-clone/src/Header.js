739c78e08acd731c17bfc36742b6b97633bf5d9f
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-05-24 22:52:45 -0600

    added icons to header and styling

--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import './Header.css'
 import MenuSharpIcon from '@mui/icons-material/MenuSharp';
+import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import KeyboardVoiceSharpIcon from '@mui/icons-material/KeyboardVoiceSharp';
+import VideoCallOutlinedIcon from '@mui/icons-material/VideoCallOutlined';
+import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
+import NotificationsNoneSharpIcon from '@mui/icons-material/NotificationsNoneSharp';
+import Avatar from '@mui/material/Avatar';
 
 function Header() {
   return (
     <div className='header'>
-      <h1>This is the HEADER</h1>
-      <MenuSharpIcon />
+      <div className='header_left'>
+        <MenuSharpIcon />
+        <img className='header_logo' src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg' alt='Youtube logo' />
+      </div>
+
+      <div className='header_input'>
+        <input type='text'/>
+        <SearchSharpIcon className='header_inputButton'/>
+        {/* <KeyboardVoiceSharpIcon /> */}
+      </div>
+
+      <div className='header_icons'>
+        <VideoCallOutlinedIcon />
+        <AppsOutlinedIcon />
+        <NotificationsNoneSharpIcon />
+        <Avatar alt="Remy Sharp" src="https://avatars.githubusercontent.com/u/94577635?v=4" />
+      </div>
     </div>
   )
 }
--- youtube-clone/src/firebase.js
+++ youtube-clone/src/firebase.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB1EocLZlLutZDpieLEym2UPiRa05pie2k",
-  authDomain: "clone-48b73.firebaseapp.com",
-  projectId: "clone-48b73",
-  storageBucket: "clone-48b73.appspot.com",
-  messagingSenderId: "255936910395",
-  appId: "1:255936910395:web:1552971ece7be2c38fb81e",
-  measurementId: "G-MFZB3R4T89"
+  apiKey: "AIzaSyACoYQG8-HOffjTYZ4xghwsLnS6QyWiCxQ",
+  authDomain: "clone-bc4cd.firebaseapp.com",
+  projectId: "clone-bc4cd",
+  storageBucket: "clone-bc4cd.appspot.com",
+  messagingSenderId: "684056717005",
+  appId: "1:684056717005:web:4c3758607faeab22fa2b84",
+  measurementId: "G-XMSQGFW6KN"
 };
