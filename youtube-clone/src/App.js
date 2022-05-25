76f946484316ed55e102f064e6b96a672da5a5c9
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-05-25 13:10:13 -0600

    started functionality of search bar

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -2,16 +2,24 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import RecommendedVideos from './RecommendedVideos';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-     <Header />
-     <div className='app_page'>
-     <Sidebar />
-    <RecommendedVideos />
-     </div>
-    </div>
+    <Router>
+      <div className="app">
+        <Header/>
+        <Routes>
+          <Route path='/search/:searchTerm' />
+          <Route path='/' element={
+            <div className='app_page'>
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          }/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import MenuSharpIcon from '@mui/icons-material/MenuSharp';
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
-import KeyboardVoiceSharpIcon from '@mui/icons-material/KeyboardVoiceSharp';
+// import KeyboardVoiceSharpIcon from '@mui/icons-material/KeyboardVoiceSharp';
 import VideoCallOutlinedIcon from '@mui/icons-material/VideoCallOutlined';
 import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
 import NotificationsNoneSharpIcon from '@mui/icons-material/NotificationsNoneSharp';
 import Avatar from '@mui/material/Avatar';
+import { Link } from 'react-router-dom';
 
 function Header() {
+  const [inputSearch, setInputSearch] = useState('')
+
   return (
     <div className='header'>
       <div className='header_left'>
         <MenuSharpIcon />
-        <img className='header_logo' src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg' alt='Youtube logo' />
+        <Link to='/'>
+          <img className='header_logo' src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg' alt='Youtube logo' />
+        </Link>
       </div>
 
       <div className='header_input'>
-        <input placeholder='Search' type='text'/>
+        <input onChange={e => setInputSearch(e.target.value)} value={inputSearch} placeholder='Search' type='text'/>
+        <Link to={`/search/${inputSearch}`}>
         <SearchSharpIcon className='header_inputButton'/>
+        </Link>
         {/* <KeyboardVoiceSharpIcon /> */}
       </div>
 
